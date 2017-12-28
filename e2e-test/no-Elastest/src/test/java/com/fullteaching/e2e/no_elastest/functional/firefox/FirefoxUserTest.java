435e3ee925925a279bce1710c0026a8c84dd81a0
gtunon <gtunon@naevatec.com>
2017-12-28 11:27:42 +0100

    ignore firefox

--- e2e-test/no-Elastest/src/test/java/com/fullteaching/e2e/no_elastest/functional/firefox/FirefoxUserTest.java
+++ e2e-test/no-Elastest/src/test/java/com/fullteaching/e2e/no_elastest/functional/firefox/FirefoxUserTest.java
@@ -2,6 +2,7 @@ package com.fullteaching.e2e.no_elastest.functional.firefox;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
@@ -10,6 +11,7 @@ import com.fullteaching.e2e.no_elastest.webDriverFactory.FirefoxFactory;
 
 import io.github.bonigarcia.wdm.FirefoxDriverManager;
 
+@Ignore
 @RunWith(Parameterized.class)
 public class FirefoxUserTest extends UserTest {
 
