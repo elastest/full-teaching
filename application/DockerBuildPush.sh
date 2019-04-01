5e657786020bc18d039bc13be5f2e114bf2d51d1
gtunon <gtunon@naevatec.com>
2019-04-01 13:54:19 +0200

    solved docker image building

--- application/DockerBuildPush.sh
+++ application/DockerBuildPush.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-cd angular-cli-project
-ng build --output-path ./../spring/backend/src/main/resources/static
-cd ../spring/backend
-mvn clean package docker:build
-docker push pablofuente/full-teaching
+cd src/main/angular
+ng build --output-path ../resources/static
+cd ../../../
+mvn -DskipTests=true clean package docker:build
+docker push elastest/demonstrator-noelastest_full-teaching:latest
